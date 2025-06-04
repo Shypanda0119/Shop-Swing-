e321d3d10c07fb43b738d2591fd91edd27cd1441
shypanda0119 <kmkm7936@naver.com>
2025-06-04 15:26:34 +0900

    간단한 프레임 구현

---
+++ shop/src/main/java/com/sinse/shop/common/config/Page.java
@@ -0,0 +1,11 @@
+package com.sinse.shop.common.config;
+
+import javax.swing.JPanel;
+
+//쇼핑몰의 모든 페이지의 최상위 객체임.. 이 객체가 JPanel을 상속
+public class Page extends JPanel{
+	
+	public Page() {
+	 setVisible(false);
+	}
+}
---
+++ shopadmin/src/main/java/com/sinse/shopadmin/common/config/Config.java
@@ -0,0 +1,25 @@
+package com.sinse.shopadmin.common.config;
+
+public class Config {
+	/*-------------------------------------
+	 각 페이지 정의
+	 --------------------------------------*/
+	public static final int MAIN_PAGE=0; //페이지
+	public static final int PRODUCT_PAGE=1; //상품 관리 페이지
+	public static final int ORDER_PAGE=2; //주문 관리 페이지
+	public static final int MEMBER_PAGE3=3;	//회원 관리 페이지
+	public static final int CUSTOMER_PAGE=4; //고객센터 관리 페이지
+	public static final int CUNFIG_PAGE=5;	
+	
+	/*-------------------------------------
+	 쇼핑몰 앱 메인 설정
+	 --------------------------------------*/
+	public static final int ADMINMAIN_WIDTH=1300;
+	public static final int ADMINMAIN_HEIGHT=800;
+	public static final int UTIL_WIDTH=ADMINMAIN_WIDTH;
+	public static final int UTIL_HEIGHT=50;
+	public static final int SIDE_WIDTH=200;
+	public static final int SIDE_HEIGHT=ADMINMAIN_HEIGHT-UTIL_HEIGHT;
+	
+	
+}
